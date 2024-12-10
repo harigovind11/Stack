6a6333fae656bcfc70425ec0d77813093b9269a3
harigovind11 <harigovindsreejith7@gmail.com>
2024-12-10 19:51:12 +0530

    bug fixes

--- Assets/Scripts/BlockMover.cs
+++ Assets/Scripts/BlockMover.cs
@@ -1,23 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 public class BlockMover : MonoBehaviour
 {
     [SerializeField] public float moveSpeed = 3f; // Speed of horizontal movement
-    [SerializeField] float direction = -1f; // Movement direction (1 for right, -1 for left)
     [SerializeField] float boundary = 3f; // The maximum distance the block can move from the center
+
+
 
     void Update()
     {
-        // Move the block horizontally
-        transform.Translate(Vector3.forward * moveSpeed * direction * Time.deltaTime);
+        bool firstSpawnPoint = GameManager.instance.useFirstSpawnPoint;
+       
+   
+        transform.Translate((!firstSpawnPoint ? Vector3.back : Vector3.right) * moveSpeed * Time.deltaTime);;
 
-        // Reverse direction if the block reaches the boundary
+  
         if (transform.position.x > boundary || transform.position.x < -boundary)
         {
-            direction *= -1f; // Flip the direction
+            //game over
+            Debug.Log("Game ");
+            //Time.timeScale = 0f;
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StackManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     [SerializeField] GameObject blockPrefab;
     [SerializeField] Transform spawnPoint1;
     [SerializeField] Transform spawnPoint2;
 
-    bool useFirstSpawnPoint = true;
+    public bool useFirstSpawnPoint = true;
+    public float placementTolerance = 0.5f;
+    public float speedIncreaseRate = 0.1f;
+
     float stackHeight;
     GameObject currentBlock;
 
-    public float placementTolerance = 0.5f;
-    public float speedIncreaseRate = 0.1f;
-    void Start()
+    void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
         SpawnBlock();
     }
 
@@ -25,34 +32,35 @@ public class StackManager : MonoBehaviour
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             PlaceBlock();
-            Debug.Log("Place0");
+            Debug.Log("Place");
         }
-        
+
 
     }
 
 
-        void SpawnBlock()
-        {
-            // Determine which spawn point to use
-            Transform selectedSpawnPoint = useFirstSpawnPoint ? spawnPoint1 : spawnPoint2;
+    void SpawnBlock()
+    {
+        // Determine which spawn point to use
+        Transform selectedSpawnPoint = useFirstSpawnPoint ? spawnPoint1 : spawnPoint2;
 
-            // Spawn a new block at the selected spawn point
-            currentBlock = Instantiate(blockPrefab, new Vector3(selectedSpawnPoint.position.x, selectedSpawnPoint.position.y + stackHeight, selectedSpawnPoint.position.z), Quaternion.identity);
+        // Spawn a new block at the selected spawn point
+        currentBlock = Instantiate(blockPrefab, new Vector3(selectedSpawnPoint.position.x, selectedSpawnPoint.position.y + stackHeight, selectedSpawnPoint.position.z), Quaternion.identity);
 
-            // Add the BlockMover script and adjust its speed
-            BlockMover mover = currentBlock.AddComponent<BlockMover>();
-            mover.moveSpeed += stackHeight * speedIncreaseRate;
+        // Add the BlockMover script and adjust its speed
+        BlockMover mover = currentBlock.AddComponent<BlockMover>();
+        mover.moveSpeed += stackHeight * speedIncreaseRate;
 
-            // Toggle the spawn point for the next block
-            useFirstSpawnPoint = !useFirstSpawnPoint;
-        }
+        // Toggle the spawn point for the next block
+        useFirstSpawnPoint = !useFirstSpawnPoint;
+    }
 
-   
+
     void PlaceBlock()
     {
         Destroy(currentBlock.GetComponent<BlockMover>());
 
+        print(Mathf.Abs(currentBlock.transform.position.x));
         if (Mathf.Abs(currentBlock.transform.position.x) > 0.5f)
         {
             Debug.Log("Game Over");
@@ -65,7 +73,9 @@ public class StackManager : MonoBehaviour
         currentBlock.transform.position = new Vector3(currentBlockPosition.x, currentBlockPosition.y, currentBlockPosition.z);
         //sfx
 
-        stackHeight += .5f;
+        stackHeight += currentBlock.transform.localScale.y;
+
+
         SpawnBlock();
     }
 }
